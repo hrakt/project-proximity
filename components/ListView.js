a32190f91a40df105b6b4b74c5d55737637a9d82
Hrak Torousian <hraktorres@gmail.com>
2021-03-04 21:55:37 -0800

    Add href for list view

--- components/ListView.js
+++ components/ListView.js
@@ -4,13 +4,16 @@ const ListView = () => {
   return (
     <div className="list">
       {list.map((item, key) => {
+        const href = `https://www.google.com/maps/dir/?api=1&destination=${item.address}%20${item.street}%20${item.city}%2C%20${item.state}%20US%20${item.zip}`;
         return (
           <div key={key} className="list-item">
-            <p>{item.name}</p>
-            <p>
-              {item.address} {item.street}, {item.city}, {item.state},{" "}
-              {item.zip}
-            </p>
+            <a href={href}>
+              <p>{item.name}</p>
+              <p>
+                {item.address} {item.street}, {item.city}, {item.state},{" "}
+                {item.zip}
+              </p>
+            </a>
           </div>
         );
       })}{" "}
