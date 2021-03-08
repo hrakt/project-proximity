fd434f07da3be909f477e9e01dba41551b652c8f
Hrak Torousian <hraktorres@gmail.com>
2021-03-08 08:55:55 -0800

    Update list to display all on load

--- components/ListView.js
+++ components/ListView.js
@@ -7,9 +7,8 @@ const ListView = ({ zipCode, radius, setRetailerCount }) => {
     return `https://www.google.com/maps/dir/?api=1&destination=${item.address}%20${item.street}%20${item.city}%2C%20${item.state}%20US%20${item.zip}`;
   };
 
-  const renderList = (zip) => {
+  const filterList = (zip) => {
     let filteredList = [];
-
     zipcodes.lookup(zip) != undefined
       ? list.map((item, key) => {
           if (zipcodes.distance(zip, item.zip) <= radius) {
@@ -27,9 +26,35 @@ const ListView = ({ zipCode, radius, setRetailerCount }) => {
           }
         })
       : null;
-    filteredList.length > 0
-      ? setRetailerCount(filteredList.length)
-      : filteredList.push(<div>No retailers found nearby</div>);
+
+    filteredList.length > 0 ? setRetailerCount(filteredList.length) : null;
+
+    return filteredList;
+  };
+
+  const renderAll = () => {
+    let renderList = [];
+    list.map((item, key) => {
+      renderList.push(
+        <div key={key} className="list-item">
+          <a href={returnLink(item)}>
+            <p>{item.name}</p>
+            <p>
+              {item.address} {item.street}, {item.city}, {item.state},{" "}
+              {item.zip}
+            </p>
+          </a>
+        </div>
+      );
+    });
+    return renderList;
+  };
+
+  const renderList = (zip) => {
+    let filteredList = [];
+
+    zip != "" ? (filteredList = filterList(zip)) : (filteredList = renderAll());
+
     return filteredList;
   };
 
