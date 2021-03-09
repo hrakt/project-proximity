d4a2abe99aaf3f962223d08318be6882a19b7969
Hrak Torousian <hraktorres@gmail.com>
2021-03-09 08:35:44 -0800

    Create own page for locator

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,15 @@
 import Head from "next/head";
-import { useState } from "react";
-import Retailer from "../components/Retailer";
-import ZipField from "../components/ZipField";
-import ViewSelector from "../components/ViewSelector";
-import Map from "../components/Map";
+
 import styles from "../styles/Home.module.css";
-import ListView from "../components/ListView";
 
 export default function Home() {
-  const [zipCode, setZipCode] = useState("");
-  const [radius, setRadius] = useState(10);
-  const [viewSelector, setViewSelector] = useState("list");
-  const [retailerCount, setRetailerCount] = useState(0);
-
   return (
     <div className={styles.container}>
       <Head>
         <title>Project Proximity</title>
       </Head>
 
-      <main className={styles.main}>
-        <ViewSelector setViewSelector={setViewSelector} />
-        <ZipField setZipCode={setZipCode} />
-        <Retailer
-          retailerCount={retailerCount}
-          zipCode={zipCode}
-          setRadius={setRadius}
-        />
-        {viewSelector === "list" ? (
-          <ListView
-            zipCode={zipCode}
-            radius={radius}
-            setRetailerCount={setRetailerCount}
-          />
-        ) : null}
-        {viewSelector === "map" ? <Map /> : null}
-      </main>
+      <main className={styles.main}></main>
 
       <footer className={styles.footer}></footer>
     </div>
---
+++ pages/locator.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Retailer from "../components/Locator/Retailer";
+import ZipField from "../components/Locator/ZipField";
+import ViewSelector from "../components/Locator/ViewSelector";
+import Map from "../components/Locator/Map";
+import ListView from "../components/Locator/ListView";
+
+import styles from "../styles/Locator.module.scss";
+
+export default function Locator() {
+  const [zipCode, setZipCode] = useState("");
+  const [radius, setRadius] = useState(10);
+  const [viewSelector, setViewSelector] = useState("list");
+  const [retailerCount, setRetailerCount] = useState(0);
+
+  return (
+    <div className={styles.locator}>
+      <ViewSelector setViewSelector={setViewSelector} />
+      <ZipField setZipCode={setZipCode} />
+      <Retailer
+        retailerCount={retailerCount}
+        zipCode={zipCode}
+        setRadius={setRadius}
+      />
+      {viewSelector === "list" ? (
+        <ListView
+          zipCode={zipCode}
+          radius={radius}
+          setRetailerCount={setRetailerCount}
+        />
+      ) : null}
+      {viewSelector === "map" ? <Map /> : null}
+    </div>
+  );
+}
