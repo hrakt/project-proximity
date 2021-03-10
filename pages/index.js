8a4c044ad91e68311732c32664fdab5f746829ff
Hrak Torousian <hraktorres@gmail.com>
2021-03-10 09:27:28 -0800

    Create footer

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
-import Head from "next/head";
-
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Project Proximity</title>
-      </Head>
-
       <main className={styles.main}></main>
 
       <footer className={styles.footer}></footer>
