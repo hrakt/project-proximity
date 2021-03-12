9bb180e4e876452f7fab0e35dd63e4d9485e86e9
Hrak Torousian <hraktorres@gmail.com>
2021-03-11 19:49:42 -0800

    Add footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,5 +1,38 @@
+import SvgIcon from "../SvgIcon/index";
+import styles from "./Footer.module.scss";
+
 const Footer = () => {
-  return <footer>footer</footer>;
+  const footerArr = [
+    { title: "About", link: "/about" },
+    { title: "Products", link: "/products" },
+    { title: "Locator", link: "/locator" },
+    { title: "Delivery", link: "/delivery" },
+    { title: "News", link: "/news" },
+  ];
+
+  return (
+    <footer className={styles.footer}>
+      <SvgIcon icon="Logo" className={styles.logo} />
+      <ul className={styles.links}>
+        {footerArr.map((link, key) => {
+          return (
+            <li className={styles.link} key={key}>
+              <a href={link.link}>{link.title}</a>
+            </li>
+          );
+        })}
+      </ul>
+      <div className={styles.socialIcons}>
+        Find us:
+        <a href="//www.facebook.com">
+          <SvgIcon icon="Facebook" className={styles.icon} />
+        </a>
+        <a href={"//www.instagram.com"}>
+          <SvgIcon icon="Instagram" className={styles.icon} />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
