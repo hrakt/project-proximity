212390fcd2472b6cc684311f07f33daa968c7c33
Hrak Torousian <hraktorres@gmail.com>
2021-03-12 17:31:23 -0800

    Make nav responsive

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,10 +1,16 @@
-import React from "react";
-
+import React, { useState } from "react";
+import cx from "classnames";
 import styles from "./Nav.module.scss";
 import Link from "next/link";
 import SvgIcon from "../SvgIcon/index";
 
 const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   const menuOptions = [
     { title: "About", href: "/about" },
     { title: "Products", href: "/products" },
@@ -13,9 +19,25 @@ const Nav = () => {
   ];
 
   return (
-    <nav className={styles.nav}>
-      <div className={styles.navContainer}>
-        <div className={styles.socialIcons}>
+    <React.Fragment>
+      <div
+        className={cx(styles.menu, {
+          [styles.menuClosed]: !menuOpen,
+        })}
+      >
+        <a href={"/"}>
+          <SvgIcon icon="Logo" className={styles.logo} />
+        </a>
+        <ul className={styles.hamburgerNav}>
+          {menuOptions.map((item, key) => {
+            return (
+              <li key={key} className={styles.hamburgerNavItem}>
+                <Link href={item.href}>{item.title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+        <div className={styles.hamburgerIcons}>
           <a href="//www.facebook.com">
             <SvgIcon icon="Facebook" className={styles.icon} />
           </a>
@@ -23,32 +45,59 @@ const Nav = () => {
             <SvgIcon icon="Instagram" className={styles.icon} />
           </a>
         </div>
-        <ul className={styles.navItems}>
-          {menuOptions.map((item, key) => {
-            return (
-              <React.Fragment>
-                {key === 2 ? (
-                  <a href={"/"}>
-                    <SvgIcon icon="Logo" className={styles.logo} />
-                  </a>
-                ) : null}
-                <li key={key} className={styles.navItem}>
-                  <Link href={item.href}>{item.title}</Link>
-                </li>
-              </React.Fragment>
-            );
-          })}
-        </ul>
-        <div className={styles.socialIcons}>
-          <a href="/delivery">
-            <SvgIcon icon="Delivery" className={styles.icon} />
-          </a>
-          <a href="/locator">
-            <SvgIcon icon="Location" className={styles.icon} />
-          </a>
-        </div>
       </div>
-    </nav>
+      <nav className={styles.nav}>
+        <div className={styles.navContainer}>
+          <div className={styles.left}>
+            <div className={styles.hamburgerWrapper} onClick={handleClick}>
+              <div
+                className={cx(styles.center, {
+                  [styles.hamburgerOpen]: menuOpen,
+                })}
+              >
+                <div></div>
+              </div>
+            </div>
+            <div className={styles.socialIcons}>
+              <a href="//www.facebook.com">
+                <SvgIcon icon="Facebook" className={styles.icon} />
+              </a>
+              <a href={"//www.instagram.com"}>
+                <SvgIcon icon="Instagram" className={styles.icon} />
+              </a>
+            </div>
+          </div>
+          <div className={styles.middle}>
+            <ul className={styles.navItems}>
+              {menuOptions.map((item, key) => {
+                return (
+                  <React.Fragment>
+                    {key === 2 ? (
+                      <a href={"/"}>
+                        <SvgIcon icon="Logo" className={styles.logo} />
+                      </a>
+                    ) : null}
+                    <li key={key} className={styles.navItem}>
+                      <Link href={item.href}>{item.title}</Link>
+                    </li>
+                  </React.Fragment>
+                );
+              })}
+            </ul>
+          </div>
+          <div className={styles.right}>
+            <div className={styles.socialIcons}>
+              <a href="/delivery">
+                <SvgIcon icon="Delivery" className={styles.icon} />
+              </a>
+              <a href="/locator">
+                <SvgIcon icon="Location" className={styles.icon} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </React.Fragment>
   );
 };
 
--- layouts/DefaultLayout.js
+++ layouts/DefaultLayout.js
@@ -11,7 +11,7 @@ const DefaultLayout = ({ children }) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
-      {/* <Nav /> */}
+      <Nav />
       <main>{children}</main>
       <Footer />
     </React.Fragment>
