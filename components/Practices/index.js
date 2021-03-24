799355c2a9cfbc0ea37c8486e002e553501d2f60
Hrak Torousian <hraktorres@gmail.com>
2021-03-23 19:01:12 -0700

    Practice edit

--- components/Practices/index.js
+++ components/Practices/index.js
@@ -1,9 +1,10 @@
+import React from "react";
 import SvgIcon from "../SvgIcon";
 import styles from "./Practices.module.scss";
 
 const Practices = ({}) => {
   const practicesHeader =
-    "Our Pactices. Understanding how important the practices in growin cannabis are, we value our methods used to achieve the best quality cannabis products in market. Every step of the way is carefully measured and planned to deliver a concisous product to our costumers.";
+    "Our Pactices. What makes us stand out is the concious growing and development of our products.";
 
   const practicesArr = [
     {
@@ -16,25 +17,34 @@ const Practices = ({}) => {
       title: "Responsibly Grown",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
+
     {
       icon: "Enviornment",
-      title: "Enviornmental Friendly Packaging",
+      title: "Enviornmental Friendly",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
   ];
 
   return (
     <div className={styles.container}>
-      <h2>{practicesHeader}</h2>
-      {practicesArr.map((practice, key) => {
-        return (
-          <div className={styles.practice}>
-            <SvgIcon iconType={practice.icon} />
-            <h4>{practice.title}</h4>
-            <span>{practice.content}</span>
-          </div>
-        );
-      })}
+      <div className={styles.haederContainer}>
+        <h2 className={styles.header}>{practicesHeader}</h2>
+      </div>
+      <div className={styles.practices}>
+        {practicesArr.map((practice, key) => {
+          return (
+            <React.Fragment key={key}>
+              <div className={styles.practice}>
+                <SvgIcon className={styles.icon} icon={practice.icon} />
+                <div className={styles.practiceText}>
+                  <h3>{practice.title}</h3>
+                  <p>{practice.content}</p>
+                </div>
+              </div>
+            </React.Fragment>
+          );
+        })}
+      </div>
     </div>
   );
 };
