5529af4a3be329243d557d9f05d1f686c1cabc55
Hrak Torousian <hraktorres@gmail.com>
2021-03-26 16:24:18 -0700

    Update news page to recieve props

--- pages/news.js
+++ pages/news.js
@@ -1,11 +1,32 @@
 import Banner from "../components/Banner";
 import DefaultLayout from "../layouts/DefaultLayout";
+import NewsPreview from "../components/News/NewsPreview";
+import Newsletter from "../components/Newsletter/Newsletter";
 
-export default function News() {
+let client = require("contentful").createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+});
+
+export async function getStaticProps() {
+  let data = await client.getEntries({
+    content_type: "article",
+  });
+
+  return {
+    props: {
+      articles: data.items,
+    },
+  };
+}
+
+export default function News({ articles }) {
   return (
     <div>
       <DefaultLayout>
         <Banner header="News" />
+        <NewsPreview articles={articles} />
+        <Newsletter />
       </DefaultLayout>
     </div>
   );
