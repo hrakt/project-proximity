2da85c32a6a70820fb4f85195f1efedcf2b9f8e5
Hrak Torousian <hraktorres@gmail.com>
2021-04-02 23:22:17 -0700

    Create contact

---
+++ components/Contact/Contact.js
@@ -0,0 +1,14 @@
+import styles from "./Contact.module.scss";
+
+const Contact = () => {
+  return (
+    <div className={styles.contact}>
+      <div>
+        <h2>We're looking to forward to hear from you!</h2>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Contact;
